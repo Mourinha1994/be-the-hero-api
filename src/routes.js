a38c8fb4259861767f4097fc278f194d441125c5
Erick Moura <erickmelodemoura17@gmail.com>
2020-03-26 20:39:57 -0300

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,20 +2,20 @@ const express = require('express');
 
 const routes = express.Router();
 
-//const OngController = require('./controllers/OngController');
-//const IncidentController = require('./controllers/IncidentController');
-//const ProfileController = require('./controllers/ProfileController');
-//const SessionController = require('./controllers/SessionController');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
-//routes.post('/sessions', SessionController.create);
+routes.post('/sessions', SessionController.create);
 
-//routes.get('/ongs', OngController.index);
-//routes.get('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+routes.get('/ongs', OngController.create);
 
-//routes.get('/incidents', IncidentController.index);
-//routes.post('/incidents', IncidentController.create);
-//routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
-//routes.get('/profile', ProfileController.index);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
